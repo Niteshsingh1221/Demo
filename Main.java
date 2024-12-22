ea54e6e34cdb6ac350e507aa872d698cd5d9f772
Lokesh-1111 <luckylokeshsharma1111@gmail.com>
2024-12-22 15:07:08 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Random random = new Random();
+        
+    
+        for (int i = 0; i < 5; i++) {
+            int randomNumber = 1000 + random.nextInt(9000);
+            System.out.println(randomNumber);
+			
+        }
+    }
+}
